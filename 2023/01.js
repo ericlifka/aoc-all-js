91b47a2026953f6a3040f97c7e89919a3ea9ec48
Eric Lifka <eric.lifka@gmail.com>
2023-12-02 04:42:33 -0500

    very misguided cleanup

--- 2023/01.js
+++ 2023/01.js
@@ -2,21 +2,22 @@ import { withInputLines } from "../utilities/with-input-lines.js"
 import { sum } from "../utilities/reducers.js"
 
 
-const getSimpleDigit = (line, pos) => line[pos] >= '0' && line[pos] <= '9' ? line[pos] : -1
+const getSimpleDigit = (line, pos) =>
+ ( line[pos] >= '0' && line[pos] <= '9' ? line[pos]
+ : -1 )
 
-const getFancyDigit = (line, pos) => {
-    if (line[pos] >= '0' && line[pos] <= '9') return line[pos]
-    else if (line[pos] == 'o' && line.slice(pos, pos+3) == "one") return "1"
-    else if (line[pos] == 't' && line.slice(pos, pos+3) == "two") return "2"
-    else if (line[pos] == 't' && line.slice(pos, pos+5) == "three") return "3"
-    else if (line[pos] == 'f' && line.slice(pos, pos+4) == "four") return "4"
-    else if (line[pos] == 'f' && line.slice(pos, pos+4) == "five") return "5"
-    else if (line[pos] == 's' && line.slice(pos, pos+3) == "six") return "6"
-    else if (line[pos] == 's' && line.slice(pos, pos+5) == "seven") return "7"
-    else if (line[pos] == 'e' && line.slice(pos, pos+5) == "eight") return "8"
-    else if (line[pos] == 'n' && line.slice(pos, pos+4) == "nine") return "9"
-    else return -1
-}
+const getFancyDigit = (line, pos) => 
+ ( line[pos] >= '0' && line[pos] <= '9' ? line[pos]
+ : line.slice(pos, pos+3) == "one" ? "1"
+ : line.slice(pos, pos+3) == "two" ? "2"
+ : line.slice(pos, pos+5) == "three" ? "3"
+ : line.slice(pos, pos+4) == "four" ? "4"
+ : line.slice(pos, pos+4) == "five" ? "5"
+ : line.slice(pos, pos+3) == "six" ? "6"
+ : line.slice(pos, pos+5) == "seven" ? "7"
+ : line.slice(pos, pos+5) == "eight" ? "8"
+ : line.slice(pos, pos+4) == "nine" ? "9"
+ : -1 )
 
 const findFirstDigit = (line, getDigit) => {
     for (let i = 0; i < line.length; i++) {
