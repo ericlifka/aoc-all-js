0cbbe34f452578df2fd0effb71d824229ef1b09c
Eric Lifka <eric.lifka@gmail.com>
2023-12-06 09:17:51 -0500

    re-do 2023 day 6 with the quadratic equation for fun

--- 2023/06.js
+++ 2023/06.js
@@ -8,16 +8,14 @@ let races = [
     [76, 1157],
     [73, 1236],
 ]
+let bigRace = [ 38677673, 234102711571236 ]
 
-const waysToWin = (time, targetDistance) => {
-    let count = 0
-    for (let i = 1; i < time; i++) {
-        if (i * (time - i) > targetDistance) {
-            count += 1
-        }
-    }
-    return count
+const doTheMath = (time, targetDistance) => {
+    let a = -1, b = time, c = -targetDistance // 0 < -x^2 + time * x - targetDistance
+    let lowerBound = (-b + Math.sqrt(b*b - 4 * a * c)) / (2 * a)
+    let upperBound = (-b - Math.sqrt(b*b - 4 * a * c)) / (2 * a)
+    return Math.ceil(upperBound - 1) - Math.floor(lowerBound + 1) + 1
 }
 
-console.log('part 1: ', races.map(([time, distance]) => waysToWin(time, distance)).reduce(multiply))
-console.log('part 2: ', waysToWin(38677673, 234102711571236))
+console.log('part 1: ', races.map(([time, distance]) => doTheMath(time, distance)).reduce(multiply))
+console.log('part 2: ', doTheMath(...bigRace))
