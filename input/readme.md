3d3fffea0b14828c6dd258234efcb2c4945ca1d9
Eric Lifka <eric.lifka@gmail.com>
2023-12-08 11:45:45 -0500

    update input method to be respectful of aoc's stance on not having puzzle input in public repos

---
+++ input/readme.md
@@ -0,0 +1,2 @@
+input files go in this folder, they should be named `<year>-<day>.txt` and then loaded with the input helpers in `utilities/with-input.js`
+text files in this folder are ignored to adhere to the aoc terms and conditions: https://adventofcode.com/2023/about
--- utilities/with-input.js
+++ utilities/with-input.js
@@ -1,6 +1,8 @@
 import fs from "node:fs"
 
-export const withInput = filename => fs.readFileSync(filename, "utf-8").trim()
-export const withInputSegments = (filename, divider) => withInput(filename).split(divider)
-export const withInputLines = filename => withInputSegments(filename, '\n')
-export const withInputChars = filename => withInputSegments(filename, '')
+const filename = (year, day) => `input/${year}-${day}.txt`
+
+export const withInput = (year, day) => fs.readFileSync(filename(year, day), "utf-8").trim()
+export const withInputSegments = (year, day, divider) => withInput(year, day).split(divider)
+export const withInputLines = (year, day) => withInputSegments(year, day, '\n')
+export const withInputChars = (year, day) => withInputSegments(year, day, '')
