a82d2d868baf5ff34d8e9cd2194af7868b6ee92f
Eric Lifka <eric.lifka@gmail.com>
2023-12-07 19:49:43 -0500

    2015 day 10

--- 2015/10.js
+++ 2015/10.js
@@ -1,25 +1,28 @@
-import { toInt } from '../utilities/parsers.js'
+import { withInput } from "../utilities/with-input.js"
 
-let puzzle = "1321131112".split('').map(toInt)
+let puzzle = withInput("2015/input/10.txt")
 
 let playRound = numbers => {
-    let newNumbers = []
+    let newNumbers = ""
     let index = 0
     while (index < numbers.length) {
          let current = numbers[index++]
          let count = 1
-         while (current == numbers[index++]) {
+         while (current == numbers[index]) {
             count++
+            index++
          }
-         newNumbers.push(count)
-         newNumbers.push(current)
+         newNumbers += count
+         newNumbers += current
     }
     return newNumbers
 }
 
-let numbers = [1, 2, 1, 1]
-console.log(numbers.join(''))
-for (let i = 0; i < 5; i++) {
-    numbers = playRound(numbers)
-    console.log(numbers.join(''))
+for (let i = 0; i < 50; i++) {
+    if (i == 40) {
+        console.log('part 1: ', puzzle.length)
+    }
+    puzzle = playRound(puzzle)
 }
+
+console.log('part 2: ', puzzle.length)
