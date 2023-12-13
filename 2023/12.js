7d7cd1bb12d681404c53f11c37be5c579a700c5a
Eric Lifka <eric.lifka@gmail.com>
2023-12-13 00:54:16 -0500

    2023 day 12

--- 2023/12.js
+++ 2023/12.js
@@ -3,19 +3,12 @@ import { sum } from "../utilities/reducers.js"
 import { toInt } from "../utilities/parsers.js"
 import { elapsed } from "../utilities/timer.js"
 
-// const group = size => "#".repeat(size)
-
 const testPattern = (pattern, str) => {
     for (let i = 0; i < str.length; i++)
         if (pattern[i] != '?' && pattern[i] != str[i])
             return false
     return true
 }
-
-// const minimumGroupLayout = groups =>
-//     groups.length == 0
-//         ? []
-//         : groups.map(group).join('.')
 
 let memoizeCache = {}
 const cacheKey = (groupStr, pattern) => `[${groupStr}]-"${pattern}"`
@@ -32,82 +25,55 @@ const permutations = (groupStr, pattern) => {
     if (groups.length == 0) { // base case ([], 5) -> [ '.....' ]
         let res = '.'.repeat(pattern.length)
         if (testPattern(pattern, res)) {
-            result = [ res ]
+            result = 1
         } else {
-            result = [ ]
+            result = 0
         }
     }
 
     else if (groups.length == 1) { // other base case ([3], 6) ->  [ '###...', '.###..', '..###.', '...###' ]
         let chunk = '#'.repeat(groups[0])
-        let perms = []
+        let validPerms = 0
         for (let i = 0; i <= pattern.length - chunk.length; i++) {
             let perm = '.'.repeat(i) + chunk + '.'.repeat(pattern.length - chunk.length - i)
             if (testPattern(pattern, perm)) {
-                perms.push(perm)
+                validPerms++
             }
         }
-        result = perms
+        result = validPerms
     }
     else {
         let [ firstGroup, ...otherGroups ] = groups
         let chunk = '#'.repeat(firstGroup)
         let sizeCap = pattern.length - (otherGroups.reduce(sum) + otherGroups.length - 1)
-        let perms = []
+        let validPerms = 0
         for (let i = 0; i < sizeCap - firstGroup; i++) {
             let groupStr = '.'.repeat(i) + chunk + '.'
             if (testPattern(pattern.slice(0, groupStr.length), groupStr)) {
-                let subPerms = permutations(otherGroups.join(','), pattern.slice(groupStr.length))
-                for (let sub of subPerms) {
-                    perms.push(groupStr + sub)
-                }
+                validPerms += permutations(otherGroups.join(','), pattern.slice(groupStr.length))
             }
         }
-        result = perms
+        result = validPerms
     }
     
     memoizeCache[key] = result
     return result
 }
 
-
-
 let ruleSets = withInputLines()
     .map( line => line.split(' ') )
 
-console.log('part 1: ', ruleSets.map(([pattern, groups]) => permutations(groups, pattern).length).reduce(sum), elapsed())
-
-
-// let ruleSets = [
-//     ["??#.?#?#???", "1,3,1"]
-// ]
-
-// ruleSets.forEach(([pattern, groups], i) => {
-//     console.log(i, ':')
-//     console.log('count: ', permutations(groups, pattern).length, '\n\n\n')
-// })
-
+console.log('part 1: ', ruleSets.map(([pattern, groups]) => permutations(groups, pattern)).reduce(sum), elapsed())
 
 let bigRuleSets = withInputLines()
     .map( line => line.split(' ') )
     .map(([p, g]) => [ [p, p, p, p, p].join('?'), [g, g, g, g, g].join(',') ])
 
-// console.log(bigRuleSets)
-
 let total = 0
 bigRuleSets.forEach(([pattern, groups], i) => {
     let result = permutations(groups, pattern)
-    console.log(i, ": ", result.length)
-    total += result.length
+    console.log(i, ": ", result)
+    total += result
 })
 
 console.log('\npart 2: ', total, elapsed())
-
-// console.log('part 2: ', bigRuleSets.map(([pattern, groups]) => permutations(groups, pattern).length).reduce(sum))
-
-// let tests = [
-//     ["?###????????".split(''), ".###.##.#...".split('')],
-//     ["?###????????".split(''), ".###..##...#".split('')],
-//     ["?#.#????????".split(''), ".###..##...#".split('')],
-// ]
-// tests.forEach(([pattern, test]) => console.log(pattern, test, testPattern(test, pattern)))
