d830c22e381648a07e045096c72dd9e1633d60aa
Eric Lifka <eric.lifka@gmail.com>
2023-12-21 11:37:21 -0500

    2023 day 21 part 2 first naive pass, won't finish

--- 2023/21.js
+++ 2023/21.js
@@ -2,30 +2,40 @@ import { withInputGrid } from "../utilities/with-input.js"
 import { reduce2d } from "../utilities/vectors.js"
 import { elapsed } from "../utilities/timer.js"
 
+const ceiling = 5000
+
 let grid = withInputGrid()
 let width = grid[0].length
 let height = grid.length
 let start = reduce2d(grid, (coord, cell, x, y) => cell == 'S' ? { x, y } : coord)
+
+const lookupCell = (x, y) => {
+    while (x < 0) x += width
+    while (x >= width) x -= width
+    while (y < 0) y += height
+    while (y >= height) y -= height
+
+    return grid[y][x]
+}
 
 let distances = {}
 let queue = [ { x: start.x, y: start.y, steps: 0 } ]
 while (queue.length > 0) {
     let { x, y, steps } = queue.shift()
 
-    if (  x >= 0 && x < width
-       && y >= 0 && y < height
-       && grid[ y ][ x ] != "#" ) // position is within the grid and not a rock
-    {
+    if ( lookupCell(x, y) != "#" ) { // position is within the grid and not a rock
         let key = `${x},${y}`
         if ( distances[key] == undefined || steps < distances[key] ) {  // position hasn't been visited before or we've reached it in fewer steps
             distances[key] = steps
             steps += 1
-            queue.push(...[
-                { x, y: y - 1, steps },
-                { x, y: y + 1, steps },
-                { x: x - 1, y, steps },
-                { x: x + 1, y, steps },
-            ])
+            if (steps <= ceiling) {
+                queue.push(...[
+                    { x, y: y - 1, steps },
+                    { x, y: y + 1, steps },
+                    { x: x - 1, y, steps },
+                    { x: x + 1, y, steps },
+                ])
+            }
         }
     }
 }
@@ -36,5 +46,13 @@ const stepsReachedIn = steps =>
             count <= steps && count % 2 == steps % 2 )
         .length
 
+console.log('generated grid', elapsed())
 
-console.log('part 1: ', stepsReachedIn(64), elapsed())
+// console.log('part 1: ', stepsReachedIn(64), elapsed())
+console.log('6:', stepsReachedIn(6), elapsed())
+console.log('10:', stepsReachedIn(10), elapsed())
+console.log('50:', stepsReachedIn(50), elapsed())
+console.log('100:', stepsReachedIn(100), elapsed())
+console.log('500:', stepsReachedIn(500))
+console.log('1000:', stepsReachedIn(1000))
+console.log('5000:', stepsReachedIn(5000))
