aeb8c5c6cde5d582772bba3927b9b0813b4778d3
Eric Lifka <eric.lifka@gmail.com>
2024-12-03 10:54:44 -0500

    silly cleanup

--- 2024/03.js
+++ 2024/03.js
@@ -8,8 +8,8 @@ console.log('2024 day 03 part 1 -',
 
 let on = true, total = 0;
 for (let instr of withInput().matchAll(/do\(\)|don't\(\)|mul\((\d{1,3}),(\d{1,3})\)/g)) {
-  if (instr[0] === "don't()")   on = false
-  else if (instr[0] === "do()") on = true
-  else if (on)                  total += Number(instr[1]) * Number(instr[2])
+  if      (instr[0] === "don't()") on = false
+  else if (instr[0] === "do()")    on = true
+  else if (on)                     total += Number(instr[1]) * Number(instr[2])
 }
 console.log('2024 day 03 part 2 -', total)
