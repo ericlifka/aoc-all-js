73a353d51b9f882d7f2a375242c39146e3403e5e
Eric Lifka <eric.lifka@gmail.com>
2024-12-03 10:57:39 -0500

    add timing

--- 2024/03.js
+++ 2024/03.js
@@ -1,5 +1,6 @@
 import {withInput} from "../utilities/with-input.js"
-import {sum} from "../utilities/reducers.js";
+import {sum} from "../utilities/reducers.js"
+import {totalElapsed} from "../utilities/timer.js"
 
 console.log('2024 day 03 part 1 -',
   [...withInput().matchAll(/mul\((\d{1,3}),(\d{1,3})\)/g)]
@@ -13,3 +14,4 @@ for (let instr of withInput().matchAll(/do\(\)|don't\(\)|mul\((\d{1,3}),(\d{1,3}
   else if (on)                     total += Number(instr[1]) * Number(instr[2])
 }
 console.log('2024 day 03 part 2 -', total)
+console.log(totalElapsed())
