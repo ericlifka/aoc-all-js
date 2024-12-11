a56506817f1e72fb05e7f1e0501798c87b718b6f
Eric Lifka <eric.lifka@gmail.com>
2024-12-11 18:46:08 -0500

    2024 day 11 part 2

--- 2024/11.js
+++ 2024/11.js
@@ -1,34 +1,63 @@
 import {withInputSegments} from "../utilities/with-input.js"
+import {sum} from "../utilities/reducers.js"
 import {totalElapsed} from "../utilities/timer.js"
 
-const input = withInputSegments(' ').map(Number)
+const digitCount = stone => Math.floor(Math.log10(stone)) + 1
+const evenDigits = stone => digitCount(stone) % 2 === 0
+const splitDigits = stone => {
+  let digits = digitCount(stone)
+  let str = `${stone}`
+  return [
+    Number(str.slice(0, digits / 2)),
+    Number(str.slice(digits / 2))
+  ]
+}
 
-const step = stones => stones.flatMap( stone => {
-  if (stone === 0)
-    return 1
-
-  let digits = Math.floor(Math.log10(stone)) + 1
-  if (digits % 2 === 0) {
-    // let magnitude = Math.pow(10, digits / 2)
-    // let midpoint = stone / magnitude
-    // let left = Math.floor(midpoint)
-    // let right = (midpoint - left) * magnitude
-    let str = `${stone}`
-    return [
-      Number(str.slice(0, digits / 2)),
-      Number(str.slice(digits / 2))
-    ]
+const incr = (obj, prop, count) => {
+  if (!obj[ prop ]) {
+    obj[ prop ] = 0
   }
+  obj[ prop ] += count
+}
 
-  return stone * 2024
-})
+const blink = stones => {
+  let next = { }
+  Object.keys(stones).forEach( key => {
+    let stone = Number(key)
+    let count = stones[ key ]
+
+    if (stone === 0) {
+      incr(next, 1, count)
+    }
+    else if (evenDigits(stone)) {
+      let [left, right] = splitDigits(stone)
+      incr(next, left, count)
+      incr(next, right, count)
+    }
+    else {
+      incr(next, stone * 2024, count)
+    }
+  })
+  return next
+}
 
 const runSim = (stones, duration) => {
   for (let i = 0; i < duration; i++) {
-    stones = step(stones)
+    stones = blink(stones)
   }
   return stones
 }
 
-console.log('2024 day 11, part 1 -', runSim(input, 25).length)
+const countStones = stones =>
+  Object.values(stones).reduce(sum)
+
+let stones = {}
+withInputSegments(' ').map(Number).forEach( stone =>
+  stones[ stone ] = 1 )
+
+let after25 = runSim(stones, 25)
+let after75 = runSim(after25, 50)
+
+console.log('2024 day 11, part 1 -', countStones(after25))
+console.log('2024 day 11, part 2 -', countStones(after75))
 console.log(totalElapsed())
