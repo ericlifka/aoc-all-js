ed61eae1b95df0c43e84a754211ae5c4f7bcae1b
Eric Lifka <eric.lifka@gmail.com>
2025-12-07 11:14:47 -0500

    day 7 non naive approach using recursion and caching

--- 2025/07.js
+++ 2025/07.js
@@ -21,20 +21,24 @@ for (let y = 1; y < grid.length; y++) {
 
 console.log('part 1 -', count, elapsed())
 
-let paths = 0
-let queue = [ { y: 0, x: grid[0].findIndex(c => c == 'S') } ]
-while (queue.length > 0) {
-  let { x, y } = queue.pop()
-  while (y < grid.length) {
-    if (grid[y][x] == '^') {
-      queue.push({ x: x + 1, y })
-      console.log({ x: x + 1, y })
-      x--
-    }
-    y++
-  }
-  paths++
-  // console.log(queue.length)
+let cache = {}
+const pathsFromPoint = (x, y) => {
+  const key = `y:${y}.x:${x}`
+  if (cache[key])
+    return cache[key]
+
+  if (y == grid.length)
+    return 1
+
+  if (grid[y][x] == '.' || grid[y][x] == 'S')
+    return cache[key] = pathsFromPoint(x, y + 1)
+
+  if (grid[y][x] == '^')
+    return cache[key] = pathsFromPoint(x + 1, y) + pathsFromPoint(x - 1, y)
+
+  console.log("didn't know what to do, x:", x, ', y:', y, ' -', grid[y][x])
 }
 
-console.log('part 2 -', paths, elapsed())
+const totalPaths = pathsFromPoint(grid[0].findIndex(c => c == 'S'), 0)
+
+console.log('part 2 -', totalPaths, elapsed())
