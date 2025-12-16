c1be3c9fb0f5cd3b49a1d2388da11549b66ec7d6
Eric Lifka <eric.lifka@gmail.com>
2025-12-15 19:13:37 -0500

    day 11

---
+++ 2025/11.js
@@ -0,0 +1,70 @@
+import { elapsed } from "../utilities/timer.js"
+import { withInputLines } from "../utilities/with-input.js"
+
+const network = withInputLines()
+  .map( line => line.split(': '))
+  .map(([ source, targets ]) => [ source, targets.split(' ') ])
+  .reduce((network, [ source, targets ]) => {
+    network[ source ] = new Set(targets)
+    return network
+  }, { out: new Set() })
+
+
+function pathsBetweenNodes(start, end) {
+  let totalPaths = 0
+  let queue = [{
+    position: start,
+    visited: new Set([ start ])
+  }]
+
+  while (queue.length > 0) {
+    let path = queue.pop()
+    for (let target of network[ path.position ]) {
+      if (target == end) {
+        totalPaths++
+      }
+      else if (!path.visited.has(target)) {
+        queue.push({
+          position: target,
+          visited: new Set([ target, ...path.visited ])
+        })
+      }
+    }
+  }
+
+  return totalPaths
+}
+
+const part1 = pathsBetweenNodes('you', 'out')
+console.log('part 1 -', part1, elapsed())
+
+function recursivePathsBetweenNodes(s, e) {
+  let cache = {}
+
+  const recur = (start, end) => {
+    const cacheKey = `${start}-${end}`
+    if (cache.hasOwnProperty(cacheKey)) {
+      return cache[ cacheKey ]
+    }
+    if (start == end) {
+      cache[ cacheKey ] = 1
+      return 1
+    }
+
+    let count = 0
+    for (let connection of network[start]) {
+      count += recur(connection, end)
+    }
+    cache[ cacheKey ] = count
+    return count
+  }
+
+  return recur(s, e)
+}
+
+const part2 =
+  recursivePathsBetweenNodes('svr', 'fft') *
+  recursivePathsBetweenNodes('fft', 'dac') *
+  recursivePathsBetweenNodes('dac', 'out')
+
+console.log('part 2 -', part2, elapsed())
